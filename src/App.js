19d9d3aec76017bb5f7825daf501bba77c4b5f5b
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-05-07 17:20:44 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       Burger Builder
+      </div>
+    );
+  }
+}
+
+export default App;
